e38822cff3fb5235e4f84e102701c4e6e3b713fa
Andersonmdev <andersonmmdev@gmail.com>
2020-03-28 12:25:08 -0300

    Validation and Tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -7,5 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
+
+/**
+ * Para Ver
+ * Padroes de codigo: ESlint, Pretter
+ * Autenticacao JWT
+ * Styled Component
+ */
